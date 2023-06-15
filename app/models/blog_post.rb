e13b83dddd6d52180dd2c8eece1dacc0afd35708
Esteban Soto <esoto074@gmail.com>
2023-06-15 11:44:42 -0600

    Add published at to blog posts Add scopes to blog posts Improved blog post form

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,6 +2,23 @@ class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
+  scope :sorted, -> { order(published_at: :desc) }
+  scope :drafts, -> { where(published_at: nil) }
+  scope :scheduled, -> { where('published_at > ?', Time.current) }
+  scope :published, -> { where('published_at <= ?', Time.current) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
+
   # has_many :comments, dependent: :destroy
 
   # before_validation :set_slug
