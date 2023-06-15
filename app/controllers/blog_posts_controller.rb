9f235fc21eabd0ba046760518bb99a0699625db3
Esteban Soto <esoto074@gmail.com>
2023-06-14 23:39:58 -0600

    Add delete functionality to blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,13 +1,12 @@
 class BlogPostsController < ApplicationController
+
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @blog_posts = BlogPost.all
   end
 
-  def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
-  end
+  def show; end
 
   def new
     @blog_post = BlogPost.new
@@ -23,12 +22,9 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  def edit
-    @blog_post = BlogPost.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -36,9 +32,20 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
