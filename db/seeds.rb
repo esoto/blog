9adb98a8cbb11ceb80f6399043c147f118ab34e0
Esteban Soto <esoto074@gmail.com>
2023-06-15 15:09:53 -0600

    Remove seeding for BlogPost

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,10 @@ user.update(
 )
 
 # Mock data 100 BlogPosts
-100.times do |i|
-  BlogPost.create(
-    title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
-    content: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5),
-    published_at: Faker::Date.between(from: 2.days.ago, to: Date.today)
-  )
-end
+# 100.times do |i|
+#   BlogPost.create(
+#     title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
+#     content: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5),
+#     published_at: Faker::Date.between(from: 2.days.ago, to: Date.today)
+#   )
+# end
