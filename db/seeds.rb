2acde9793ac35ee88fcb380234631abc0badebd2
Esteban Soto <esoto074@gmail.com>
2023-06-15 15:03:13 -0600

    Seed prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,12 @@ user.update(
   password: "1234567890",
   password_confirmation: "1234567890"
 )
+
+# Mock data 100 BlogPosts
+100.times do |i|
+  BlogPost.create(
+    title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
+    content: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 5),
+    published_at: Faker::Date.between(from: 2.days.ago, to: Date.today)
+  )
+end
